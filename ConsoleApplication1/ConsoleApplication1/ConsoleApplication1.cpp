aa743037c78430c52ff108e883ab8f83c91e53fd
AndriyOd <andriy.kalyapin@gmail.com>
2019-08-21 00:34:04 +0300

    Change sorting method to 'bubble' method

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -13,6 +13,8 @@ int main()
 	int iArraySize = 8;
 	int iMin = 0;
 	int i = 0;
+	int j = 0;
+	int k = 0;
 	int iTemp = 0;
 	int n = 0;
 
@@ -49,49 +51,28 @@ int main()
 	std::cout << "}\n\n";
 
 
-	// Sorting the array ar1[4]
-	std::cout << "Sorting...\n\n";
-	while (iLowBorder<iArraySize - 1)
+	// Sorting the array ar1[10]. BUBBLE METHOD
+	std::cout << "Sorting... BUBBLE METHOD ....\n";
+	for (i = 0; i < iArraySize - 1; i++)
 	{
-		iMin = ar1[iLowBorder];
-		n = iLowBorder;
-		for (i = iLowBorder + 1; i<iArraySize; i++)
+		for (j = 0; j < iArraySize - 1 - i; j++)
 		{
-			if (ar1[i] < iMin)
+			if (ar1[j] > ar1[j + 1])
 			{
-				n = i;
-				iMin = ar1[i];
+				iTemp = ar1[j];
+				ar1[j] = ar1[j + 1];
+				ar1[j + 1] = iTemp;
 			}
+			//Printing the arrray
+			std::cout << "Step " << i << "." << j << ". ar1[" << iArraySize << "]={";
+			for (k = 0; k < iArraySize; k++)
+			{
+				std::cout << ar1[k];
+				if (k < iArraySize - 1)
+					std::cout << ",";
+			}
+			std::cout << "}\n\n";
 		}
-
-		if (n != iLowBorder)
-		{
-			iTemp = ar1[iLowBorder];
-			ar1[iLowBorder] = ar1[n];
-			ar1[n] = iTemp;
-		}
-
-		
-		//just delete the previous line
-		//--- Let's add some extra lines to view interim information ---
-		//---------------- Beginning ----------------
-		std::cout << "Step " <<iLowBorder<< "\n";
-		std::cout << "ar[" << n <<"] = "<<ar1[iLowBorder]<<" was the Min\n";
-
-		//Printing the arrray
-		std::cout << "And now ar1[" << iArraySize << "]={";
-		for (i = 0; i < iArraySize; i++)
-		{
-		std::cout << ar1[i];
-		if (i < iArraySize - 1)
-		std::cout << ",";
-		}
-		std::cout << "}\n\n";
-		//---------------- End ----------------
-		//just delete the next line
-		
-
-		iLowBorder++;
 	}
 
 	//Printing the arrray
