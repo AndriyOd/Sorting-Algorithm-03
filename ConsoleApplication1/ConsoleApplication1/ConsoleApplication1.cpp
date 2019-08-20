b0b44318fe33a78f89c6bf98bc17c2002c5071ab
AndriyOd <andriy.kalyapin@gmail.com>
2019-08-21 00:12:52 +0300

    Added extra lines (which are commented out)

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -7,72 +7,59 @@
 
 int main()
 {
-	int a=0;
+	int a = 0;
 
 	int iLowBorder = 0;
-	int iArrayVolume = 8;
+	int iArraySize = 8;
 	int iMin = 0;
 	int i = 0;
 	int iTemp = 0;
 	int n = 0;
 
-	int ar1[10] = {1,2,3,4};
-	std::cout << "Hello, World\n";
-	//std::cin >>a;
-	//std::cout << "\na="<<a<<"\n";
-	//std::cout << "Initially ar1[4]={"<<ar1[0]<<","<<ar1[1]<< ","<<ar1[2]<<","<<ar1[3] << "}\n";
+	int ar1[10] = { 1,2,3,4 };
+	std::cout << "Hello, World!\n\n";
 
 	//Printing the arrray
-	std::cout << "Initially ar1[" << iArrayVolume << "]={";
-	for (i = 0; i < iArrayVolume; i++)
+	std::cout << "Initially array ar1[" << iArraySize << "]={";
+	for (i = 0; i < iArraySize; i++)
 	{
 		std::cout << ar1[i];
-		if (i < iArrayVolume - 1)
+		if (i < iArraySize - 1)
 			std::cout << ",";
 	}
-	std::cout << "}\n";
+	std::cout << "}\n\n";
 
-	// Entering the elements of the array ar1[4]
-	for (i = 0; i < iArrayVolume; i++)
+	// Input the elements of the array ar1[4]
+	std::cout << "Now input the elements of the array:\n";
+	for (i = 0; i < iArraySize; i++)
 	{
-		std::cout << "Enter ar["<<i <<"]\n";
+		std::cout << "Input ar[" << i << "]\n";
 		std::cin >> ar1[i];
 
 	}
 
 	//Printing the arrray
-	std::cout << "And now ar1[" << iArrayVolume << "]={";
-	for (i = 0; i < iArrayVolume; i++)
+	std::cout << "And now array ar1[" << iArraySize << "]={";
+	for (i = 0; i < iArraySize; i++)
 	{
 		std::cout << ar1[i];
-		if (i < iArrayVolume - 1)
+		if (i < iArraySize - 1)
 			std::cout << ",";
 	}
-	std::cout << "}\n";
-	//std::cout << "And now ar1[4]={" << ar1[0] << "," << ar1[1] << "," << ar1[2] << "," << ar1[3] << "}\n";
+	std::cout << "}\n\n";
 
-	/*
-	std::cout << "Enter ar1 elements:\nar1[0]=";
-	std::cin >> ar1[0];
-	std::cout << "Enter ar1 elements:\nar1[1]=";
-	std::cin >> ar1[1];
-	std::cout << "Enter ar1 elements:\nar1[2]=";
-	std::cin >> ar1[2];
-	std::cout << "Enter ar1 elements:\nar1[3]=";
-	std::cin >> ar1[3];
-	*/
 
 	// Sorting the array ar1[4]
-	std::cout << "Sorting...\n";
-	while (iLowBorder<iArrayVolume-1)
+	std::cout << "Sorting...\n\n";
+	while (iLowBorder<iArraySize - 1)
 	{
 		iMin = ar1[iLowBorder];
 		n = iLowBorder;
-		for (i=iLowBorder+1; i<iArrayVolume; i++)
+		for (i = iLowBorder + 1; i<iArraySize; i++)
 		{
 			if (ar1[i] < iMin)
 			{
-				n=i;
+				n = i;
 				iMin = ar1[i];
 			}
 		}
@@ -84,26 +71,40 @@ int main()
 			ar1[n] = iTemp;
 		}
 
-		//std::cout << "Step " <<iLowBorder<< "\n";
-		//std::cout << "ar[" << n <<"] = "<<ar1[iLowBorder]<<" was the Min\n";
-		//std::cout << "ar1[4]={" << ar1[0] << "," << ar1[1] << "," << ar1[2] << "," << ar1[3] << "}\n";
+		/*
+		//just delete the previous line
+		//--- Let's add some extra lines to view interim information ---
+		//---------------- Beginning ----------------
+		std::cout << "Step " <<iLowBorder<< "\n";
+		std::cout << "ar[" << n <<"] = "<<ar1[iLowBorder]<<" was the Min\n";
+
+		//Printing the arrray
+		std::cout << "And now ar1[" << iArraySize << "]={";
+		for (i = 0; i < iArraySize; i++)
+		{
+		std::cout << ar1[i];
+		if (i < iArraySize - 1)
+		std::cout << ",";
+		}
+		std::cout << "}\n\n";
+		//---------------- End ----------------
+		//just delete the next line
+		*/
+
 		iLowBorder++;
-	} 
-	//while (iLowBorder<iArrayVolume);
+	}
 
 	//Printing the arrray
-	std::cout << "And after sorting ar1[" << iArrayVolume << "]={";
-	for (i = 0; i < iArrayVolume; i++)
+	std::cout << "And after sorting array ar1[" << iArraySize << "]={";
+	for (i = 0; i < iArraySize; i++)
 	{
 		std::cout << ar1[i];
-		if (i < iArrayVolume - 1)
+		if (i < iArraySize - 1)
 			std::cout << ",";
 	}
-	std::cout << "}\n";
-	//std::cout << "And after sorting ar1[4]={" << ar1[0] << "," << ar1[1] << "," << ar1[2] << "," << ar1[3] << "}\n";
-
+	std::cout << "}\n\n";
 
 	std::cout << "Press any key for exiting\n";
 	std::cin >> a;
-    return 0;
+	return 0;
 }
